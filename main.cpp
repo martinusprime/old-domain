e2a3e7d83bbca70f015b470e388696d13694864c
Etienne Cordonnier <cordonnier.etienne@gmail.com>
2015-05-25 18:18:18 +0200

    Ajoute catch block global pour les exceptions

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <SFML/Graphics.hpp>
 #include "Game_Manager.h"
+#include <iostream>
+
 using namespace sf;
 
 
@@ -10,21 +12,24 @@ using namespace sf;
 
 int main()
 {
-    RenderWindow app(VideoMode(1600, 920), "SFML window" );
+    try {
+        RenderWindow app(VideoMode(1600, 920), "SFML window" );
+        //app.setFramerateLimit(60);
+        Game_Manager game_manager1;
 
-
-    Game_Manager game_manager1;
-    // Create the main window
-
-    //app.setMouseCursorVisible(false);
-    game_manager1.init(&app);
-    // Start the game loop
-    while (app.isOpen() )
-    {
-        game_manager1.update();
-        game_manager1.draw();
+        // Create the main window
+        //app.setMouseCursorVisible(false);
+        game_manager1.init(&app);
+        // Start the game loop
+        while (app.isOpen() )
+        {
+            game_manager1.update();
+            game_manager1.draw();
+        }
+    } catch (const std::exception &ex) {
+        std::cerr << "Terminate program with exception " << ex.what();
+        return EXIT_FAILURE;
     }
-
 
     return EXIT_SUCCESS;
 }
