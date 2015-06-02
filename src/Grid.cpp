ec5cff10ed82c38fe7a6b3fca2c0668eee04646f
martinusprime <martin.relais@yahoo.fr>
2015-06-02 23:00:21 +0200

    add resource struct

--- src/Grid.cpp
+++ src/Grid.cpp
@@ -18,6 +18,7 @@ Grid::Grid(unsigned int width, unsigned int heigth, sf::View *view1, sf::RenderW
     : m_grid(width, std::vector<Tile>(heigth))
     , m_view1(view1)
     , m_app(app)
+	, m_resource(1)
 {
              clock1.restart();
 
@@ -38,7 +39,11 @@ void Grid::loadFiles()
     My_Sprite resource_sprite0;
     m_sprite_creator1.init(m_app, m_view1);
     resource_sprite0.init(m_app, "ressources/wood_ressource.png", m_view1);
-	m_resource_sprites.init(m_app, m_sprite_creator1.create_resources(0), m_view1, 128, 5, 1);
+	m_resource[0].sprite.init(m_app, m_sprite_creator1.create_resources(0), m_view1, 128, 5, 1);
+	m_resource[0].name = m_sprite_creator1.get_resource_name();
+	m_resource[0].solidity = m_sprite_creator1.get_solidity();
+	m_resource[0].flexibility = m_sprite_creator1.get_flexibility();
+
     for(int i = 0; i < 10; i++)
     {
         stringstream ss;
@@ -66,9 +71,9 @@ void Grid::draw()
 			{
 				(*this)(i, j).draw((*this)(i, j).type, (*this)(i, j).x_pos, (*this)(i, j).y_pos);
 			}
-			if ((*this)(i, j).ressource_type == RSC_WOOD  && i < 5 && j < 5)
+			if ((*this)(i, j).ressource_type == RSC_WOOD  && i < 25 && j < 25)
 			{
-				m_resource_sprites.draw_tile(((*this)(i, j).x_pos - (*this)(i, j).y_pos)* (Tile::tile_size.x / 2), ((*this)(i, j).x_pos + (*this)(i, j).y_pos)* (Tile::tile_size.y / 2), (*this)(i, j).random_pattern);
+				m_resource[0].sprite.draw_tile(((*this)(i, j).x_pos - (*this)(i, j).y_pos)* (Tile::tile_size.x / 2), ((*this)(i, j).x_pos + (*this)(i, j).y_pos)* (Tile::tile_size.y / 2), (*this)(i, j).random_pattern);
 
 
 			}
